7ca69d4b7619b4928049144483bb42fc7d83d549
rehius <128579193+rehius@users.noreply.github.com>
2023-05-26 22:44:24 +0000

    Update main.c

--- main.c
+++ main.c
@@ -21,10 +21,10 @@
 
 bool write_payload();
 
-// overclock to 200 MHz, pretty stable & enough for the glitch
+// overclock to 300 MHz
 void init_system() {
     vreg_set_voltage(VREG_VOLTAGE_1_30);
-	set_sys_clock_khz(200000, true);
+	set_sys_clock_khz(300000, true);
 }
 
 // filled within "fast check" on eMMC init
